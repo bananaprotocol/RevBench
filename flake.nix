e20d84baa4153c39ea576d3d91f9f388d2d7eb63
bananaprotocol <22718694+bananaprotocol@users.noreply.github.com>
2025-11-04 17:38:20 +0100

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "RevBench flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python3
+          uv
+          gcc
+          clang
+          ghidra
+        ];
+
+        shellHook = ''
+          unset PYTHONPATH
+          uv sync
+          . .venv/bin/activate
+        '';
+      };
+    };
+}
