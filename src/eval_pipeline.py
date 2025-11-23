8b5cf0b72c5da69199a90b154a5b1f6b32e59cb2
bananaprotocol <22718694+bananaprotocol@users.noreply.github.com>
2025-11-23 15:22:54 +0100

    fix(eval): add relevant headers for test harness

--- src/eval_pipeline.py
+++ src/eval_pipeline.py
@@ -67,7 +67,7 @@ class RevBench:
 
     def functional_check(self, generated_func, test_harness_main):
         # add common headers
-        headers = "#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n#include <math.h>\n#include <stdbool.h>\n"
+        headers = "#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n#include <math.h>\n#include <stdbool.h>\n#include <assert.h>#include <stdarg.h>\n"
 
         # inject standard ghidra typedefs
         ghidra_defs = "typedef unsigned int uint; typedef unsigned char byte; typedef unsigned long ulong;\n"
